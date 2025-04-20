1b000f96a1847ff834646dda048689aeca09948a
nehalkubade <kubade9639@gmail.com>
2025-04-20 19:10:09 +0530

    changes

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -234,6 +234,7 @@ const Calendar: React.FC = () => {
               <input
                 id="event-title"
                 type="text"
+                autoComplete="off"
                 value={eventTitle}
                 onChange={(e) => setEventTitle(e.target.value)}
                 className="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
